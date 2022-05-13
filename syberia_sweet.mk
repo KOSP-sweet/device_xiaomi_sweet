d8456915d8997dcb41f7149395d379680e679cd9
anupam41 <anupamsamanta41@gmail.com>
2022-05-13 12:21:47 +0530

    sweet: Init syberia

--- syberia_sweet.mk
+++ syberia_sweet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The syberiaOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common syberia stuff.
+$(call inherit-product, vendor/syberia/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_FACE_UNLOCK := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := syberia_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
