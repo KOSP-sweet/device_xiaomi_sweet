56ded403091a2bdb03d1ca92adaed8516307330e
anupam41 <anupamsamanta41@gmail.com>
2022-05-13 12:21:47 +0530

    sweet: Init syberia

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The syberiaOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/syberia_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    syberia_sweet-user \
+    syberia_sweet-userdebug \
+    syberia_sweet-eng
