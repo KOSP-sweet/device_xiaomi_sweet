64af1e791cc600bf3def266d0cb49f6865964ae0
anupam41 <anupamsamanta41@gmail.com>
2022-06-08 13:27:31 +0530

    sweet: Init Spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The sparkOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/spark_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    spark_sweet-user \
+    spark_sweet-userdebug \
+    spark_sweet-eng
